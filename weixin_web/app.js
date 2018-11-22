a2182c7b34e88e6374ee33f0b6f2e36bd4123338
zgcra <44994440+zgcra@users.noreply.github.com>
2018-11-22 14:48:07 +0800

    Add files via upload

---
+++ weixin_web/app.js
@@ -0,0 +1,6 @@
+//app.js
+App({
+  onLaunch: function () {
+  },
+  globalData:'I am global data'
+})
---
+++ weixin_web/pages/index/index.js
@@ -0,0 +1,16 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    picture:[
+      "../input/swiper1.jpg",
+      "../input/swiper2.jpg",
+      "../input/swiper3.jpg",
+      "../input/swiper4.jpg"
+    ]
+  },
+  onLoad: function () {
+  },
+})
